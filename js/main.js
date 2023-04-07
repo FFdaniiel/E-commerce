64a6d0883530201480552a51e433df43787fb14b
Daniel Sanduay <daniel_alexis_12@hotmail.com>
2023-04-07 19:57:49 -0300

    Agregando librerias y fix menores

--- js/main.js
+++ js/main.js
@@ -1,194 +1,20 @@
-// Productos
-const productos = [
+let productos = []
 
-    // Abrigos ------------------------------------
-
-    {
-        id: 'abrigo-01',
-        titulo: 'Abrigo 01',
-        imagen: './img/abrigos/01.jpg',
-        categoria: {
-            nombre:'Abrigos',
-            id:'abrigos'
-        },
-        precio: 1000,
-    },
-    {
-        id: 'abrigo-02',
-        titulo: 'Abrigo 02',
-        imagen: './img/abrigos/02.jpg',
-        categoria: {
-            nombre:'Abrigos',
-            id:'abrigos'
-        },
-        precio: 1000,
-    },
-    {
-        id: 'abrigo-03',
-        titulo: 'Abrigo 03',
-        imagen: './img/abrigos/03.jpg',
-        categoria: {
-            nombre:'Abrigos',
-            id:'abrigos'
-        },
-        precio: 1000,
-    },
-    {
-        id: 'abrigo-04',
-        titulo: 'Abrigo 04',
-        imagen: './img/abrigos/04.jpg',
-        categoria: {
-            nombre:'Abrigos',
-            id:'abrigos'
-        },
-        precio: 1000,
-    },
-    {
-        id: 'abrigo-05',
-        titulo: 'Abrigo 05',
-        imagen: './img/abrigos/05.jpg',
-        categoria: {
-            nombre:'Abrigos',
-            id:'abrigos'
-        },
-        precio: 1000,
-    },
-
-    // Camisetas ------------------------------------
-    {
-        id: 'camiseta-01',
-        titulo: 'camiseta 01',
-        imagen: './img/camisetas/01.jpg',
-        categoria: {
-            nombre:'Camisetas',
-            id:'camisetas'
-        },
-        precio: 1000,
-    },
-    {
-        id: 'camiseta-02',
-        titulo: 'camiseta 02',
-        imagen: './img/camisetas/02.jpg',
-        categoria: {
-            nombre:'Camisetas',
-            id:'camisetas'
-        },
-        precio: 1000,
-    },
-    {
-        id: 'camiseta-03',
-        titulo: 'camiseta 03',
-        imagen: './img/camisetas/03.jpg',
-        categoria: {
-            nombre:'Camisetas',
-            id:'camisetas'
-        },
-        precio: 1000,
-    },
-    {
-        id: 'camiseta-04',
-        titulo: 'camiseta 04',
-        imagen: './img/camisetas/04.jpg',
-        categoria: {
-            nombre:'Camisetas',
-            id:'camisetas'
-        },
-        precio: 1000,
-    },
-    {
-        id: 'camiseta-05',
-        titulo: 'camiseta 05',
-        imagen: './img/camisetas/05.jpg',
-        categoria: {
-            nombre:'Camisetas',
-            id:'camisetas'
-        },
-        precio: 1000,
-    },
-    {
-        id: 'camiseta-06',
-        titulo: 'camiseta 06',
-        imagen: './img/camisetas/06.jpg',
-        categoria: {
-            nombre:'Camisetas',
-            id:'camisetas'
-        },
-        precio: 1000,
-    },
-    {
-        id: 'camiseta-07',
-        titulo: 'camiseta 07',
-        imagen: './img/camisetas/07.jpg',
-        categoria: {
-            nombre:'Camisetas',
-            id:'camisetas'
-        },
-        precio: 1000,
-    },
-    {
-        id: 'camiseta-08',
-        titulo: 'camiseta 08',
-        imagen: './img/camisetas/08.jpg',
-        categoria: {
-            nombre:'Camisetas',
-            id:'camisetas'
-        },
-        precio: 1000,
-    },
-    // Pantalones ------------------------------------
-    {
-        id: 'pantalon-01' ,
-        titulo: 'pantalon 01',
-        imagen: './img/pantalones/01.jpg',
-        categoria: {
-            nombre: 'Pantalones',
-            id: 'pantalones',
-        }
-    },
-    {
-        id: 'pantalon-02' ,
-        titulo: 'pantalon 02',
-        imagen: './img/pantalones/02.jpg',
-        categoria: {
-            nombre: 'Pantalones',
-            id: 'pantalones',
-        }
-    },
-    {
-        id: 'pantalon-03' ,
-        titulo: 'pantalon 03',
-        imagen: './img/pantalones/03.jpg',
-        categoria: {
-            nombre: 'Pantalones',
-            id: 'pantalones',
-        }
-    },
-    {
-        id: 'pantalon-04' ,
-        titulo: 'pantalon 04',
-        imagen: './img/pantalones/04.jpg',
-        categoria: {
-            nombre: 'Pantalones',
-            id: 'pantalones',
-        }
-    },
-    {
-        id: 'pantalon-05' ,
-        titulo: 'pantalon 05',
-        imagen: './img/pantalones/05.jpg',
-        categoria: {
-            nombre: 'Pantalones',
-            id: 'pantalones',
-        }
-    },
-];
-
+fetch('./js/productos.json')
+    .then(response => response.json())
+    .then(data => {
+        productos = data;
+        cargarProductos(productos);
+    })
 const  contenedorProductos = document.querySelector('#contenedor-productos');
 const  btnCategorias = document.querySelectorAll('.btn-categoria');
 const  tituloPrincipal = document.querySelector('#titulo-principal');
 let  btnAgregar = document.querySelectorAll('.producto-agregar');
 let  numerito = document.querySelector('#numero');
 
+btnCategorias.forEach(boton => boton.addEventListener('click' , () => {
+    aside.classList.remove('aside-visible');
+}));
 
 function cargarProductos(productosElegidos) {
 
@@ -256,6 +82,26 @@ if(productosEnCarritoLS){
 }
 
 function agregarAlCarrito(e) {
+
+    Toastify({
+        text: "Producto agregado!",
+        duration: 3000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #4b33a8, #785ce9",
+          borderRadius: "2rem",
+          textTransform: "upperCase",
+          fontSize: ".75rem",
+        },
+        offset: {
+            x: "1.5rem", // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+            y: "1.5rem" // vertical axis - can be a number or a string indicating unity. eg: '2em'
+          },
+        onClick: function(){} // Callback after click
+      }).showToast();
 
     const idBoton = e.currentTarget.id;
     const productoAgregado = productos.find(producto => producto.id === idBoton);
